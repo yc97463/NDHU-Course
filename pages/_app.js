51c9e37ebfed7c6d9531527f01edd0ec377c1f5e
yc97463 <youualan87@gmail.com>
2023-01-07 14:01:14 +0800

    feat(_app): disable global css

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
-import '../styles/globals.css'
+// import '../styles/globals.css'
+import "@fontsource/public-sans";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
