c3c8eca2cb56c5caee060e178022f96978835ff0
yc97463 <hi@imych.one>
2025-03-13 21:10:11 +0800

    feat(config): set basePath and assetPrefix for production environment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: isProd ? "/ndhu-course-crawler" : "",
+  assetPrefix: isProd ? "/ndhu-course-crawler/" : "",
 };
 
 export default nextConfig;
